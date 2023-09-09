1ebfd94e02620ec81e1ee38d5c10050ae25e32a6
Максим <dispeloff@gmail.com>
2023-09-09 19:43:02 +1000

    update

--- OpenNastenka.lua
+++ OpenNastenka.lua
@@ -7,3 +7,31 @@ local io = require("io")
 
 local gpu = com.gpu
 local cb = com.chat_box
+
+-- event "chat_message"
+function ReadChat()
+  local _, _, Nickname, Message = event.pull(1, "chat_message", nil, "dispeloff") -- msg принимается только от меня
+  if Message == nil then
+  return
+
+  elseif Message == "@help" then
+    cb.say("§bСписок доступных команд: \n   §c1.§a help §6 - Список доступных команд \n   §c1.§a stop §6 - Остановка бота\n")
+  return
+
+  elseif Message == "@stop" then
+    cb.say("§cМоя Остановочка")
+    os.exit()
+  end
+end
+
+-- main loop function
+function MainLoop()
+  while true do
+    ReadChat()
+  end
+end
+
+cb.say("§6Привет dispeloff!")
+os.sleep(0.3)
+
+MainLoop()
