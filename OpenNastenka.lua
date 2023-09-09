dee69cd5651e9d90d4222cf9b5e6ba9096de4c74
Максим <dispeloff@gmail.com>
2023-09-09 19:20:08 +1000

    update

---
+++ OpenNastenka.lua
@@ -0,0 +1,9 @@
+local com = require("component")
+local computer = require("computer")
+local term = require("term")
+local event = require("event")
+local fs = require("filesystem")
+local io = require("io")
+
+local gpu = com.gpu
+local cb = com.chat_box
